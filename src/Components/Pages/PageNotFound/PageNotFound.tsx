4f4f33af6658982fb0dda81849fe805e6655a0c1
Maltemork <malte.sorensen@protonmail.com>
2024-05-20 01:13:49 +0200

    Fixed wrong routes and created a 404 page (also routes to 404 page if you try to access site you are not authorized for)

---
+++ src/Components/Pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,9 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>404 - Page not found</h1>
+      <h2>It appears the page you were trying to access does not exist...</h2>
+      <p>You can use the sidebar to go back to one of the known pages.</p>
+    </div>
+  );
+}
